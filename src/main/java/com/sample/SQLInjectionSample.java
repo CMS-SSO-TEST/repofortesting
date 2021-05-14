662085853592722f19b39dba14a3b4ea48c5baff
Ramesh Mahimalur <rmahimalur@nimbusconsulting.com>
2021-05-14 11:54:34 -0400

    updated codeql file

--- src/main/java/com/sample/SQLInjectionSample.java
+++ src/main/java/com/sample/SQLInjectionSample.java
@@ -13,7 +13,7 @@ public class SQLInjectionSample {
     DataSource dataSource;
     private String url = "jdbc:mysql://localhost/test";
     private String username = "root";
-    private String password = "";
+    private String password = "root1234";
 
 
     private Connection getConn() throws SQLException {
